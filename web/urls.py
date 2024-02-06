6f1025c7b6dca0fed5f3ed8ab3e4d47a10685a7d
asir <asirm0658@gmail.com>
2024-02-06 21:25:38 +0530

    first commit

---
+++ web/urls.py
@@ -0,0 +1,11 @@
+# Create web/urls.py and paste the following
+from django.urls import path
+from . import views
+
+
+app_name = "web"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    
+]
---
+++ web/views.py
@@ -0,0 +1,14 @@
+# Edit web/views.py
+from django.shortcuts import render
+
+
+
+
+def index(request):
+    context = {"is_index": True}
+    return render(request, "web/index.html", context)
+
+
+def about(request):
+    context = {"is_about": True}
+    return render(request, "web/about.html", context)
